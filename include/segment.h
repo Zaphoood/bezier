0ba74795e9c1e60bffe25b4e19809f9bab41a152
Mathis Weber <mathis.c.weber@web.de>
2022-12-31 14:10:28 +0100

    refactor: clean up Segment

--- include/segment.h
+++ include/segment.h
@@ -7,18 +7,18 @@
 #include "visuals.h"
 
 class Segment : public VisualObject {
-private:
-	BezierPoint* m_p0;
-	BezierPoint* m_p1;
-	std::vector<Vec2> m_curve; // The points on the resulting bezier curve
-	int m_selPoint = -1; // Which points is currently selected
-	int pointSize = 8;
-	std::vector<Vec2> getDefiningPoints();
-	std::vector<Vec2> renderCurve();
-	std::vector<Vec2> renderCurveAdaptive();
-	std::vector<Vec2> renderCurve(int steps);
-public:
-	void updateCurve();
-	Segment(BezierPoint* p0, BezierPoint* p1);
-	void draw(SDL_Renderer* renderer) override;
+  private:
+    BezierPoint* m_p0;
+    BezierPoint* m_p1;
+    // The points on the resulting bezier curve
+    std::vector<Vec2> m_curve;
+    std::vector<Vec2> getDefiningPoints();
+    void renderCurve();
+    void renderCurveAdaptive();
+    void renderCurve(int steps);
+
+  public:
+    void updateCurve();
+    Segment(BezierPoint* p0, BezierPoint* p1);
+    void draw(SDL_Renderer* renderer) override;
 };
--- src/segment.cpp
+++ src/segment.cpp
@@ -1,59 +1,62 @@
 #include "segment.h"
 
+const int MAX_STEP_DISTANCE = 8;
+
 Segment::Segment(BezierPoint* p0, BezierPoint* p1) 
 : m_p0(p0), m_p1(p1) {
 	updateCurve();
 }
+
 void Segment::draw(SDL_Renderer* renderer) {
 	// Draw curve
 	SDL_SetRenderDrawColor(renderer, 151, 64, 53, SDL_ALPHA_OPAQUE);
-	for (std::vector<Vec2>::iterator it = m_curve.begin(), second_to_last = --m_curve.end();
-		it != second_to_last; ++it) {
-
-		SDL_RenderDrawLine(renderer, it->x, it->y, std::next(it, 1)->x, std::next(it, 1)->y);
+	for (std::vector<Vec2>::iterator it = m_curve.begin(); it != std::prev(m_curve.end()); ++it) {
+		SDL_RenderDrawLine(renderer, it->x, it->y, std::next(it)->x, std::next(it)->y);
 	}
 }
+
 void Segment::updateCurve() {
-	// Renders the curve and stores it in m_curve
-	m_curve = renderCurve();
+	renderCurve();
 }
+
 std::vector<Vec2> Segment::getDefiningPoints() {
 	return std::vector<Vec2>{ Vec2(m_p0->getPosition()), Vec2(m_p0->getRightArm()), Vec2(m_p1->getLeftArm()), Vec2(m_p1->getPosition()) };
 }
-std::vector<Vec2> Segment::renderCurve() {
-	return renderCurveAdaptive();
+
+void Segment::renderCurve() {
+	renderCurveAdaptive();
 }
-std::vector<Vec2> Segment::renderCurveAdaptive() {
-	// Adjust the number of steps to the distance between the start and end point
-	int dist = 0;
+
+void Segment::renderCurveAdaptive() {
+	// Adjust the number of steps to the maximum distance distance between two points
+	int max_distance = 0;
 	std::vector<Vec2> points = getDefiningPoints();
 
-	for (std::vector<Vec2>::iterator it = points.begin(), second_to_last = --points.end();
-		it != second_to_last; ++it) {
-		Vec2 distVec = *it - *std::next(it, 1);
-		dist = std::max(abs(distVec.x) + abs(distVec.y), dist); // Using taxicab distance for a rough estimate of the distance
+	for (std::vector<Vec2>::iterator it = points.begin(); it != std::prev(points.end()); ++it) {
+		Vec2 distVec = *it - *std::next(it);
+    // Using taxicab distance for a rough estimate of the distance
+		max_distance = std::max(abs(distVec.x) + abs(distVec.y), max_distance);
 	}
-	int n_steps = dist / 8;
-	return renderCurve(n_steps);
+
+	renderCurve(max_distance / MAX_STEP_DISTANCE);
 }
-std::vector<Vec2> Segment::renderCurve(int steps) {
-	std::vector<Vec2> curve;
-	float progr = 0.0;
-	float stepSize = (float)(1.0) / steps;
+
+void Segment::renderCurve(int steps) {
+	float t;
+
+	m_curve.clear();
 	for (int i = 0; i <= steps; i++) {
+    t = (float) i / steps;
+
 		std::vector<Vec2> points = getDefiningPoints();
 		std::vector<Vec2> points2;
 		std::vector<Vec2> points3;
-		for (std::vector<Vec2>::iterator it = points.begin(), second_to_last = --points.end();
-			it != second_to_last; ++it) {
-			points2.push_back(AtoBProg(*it, *std::next(it, 1), progr));
+		for (std::vector<Vec2>::iterator it = points.begin(); it != std::prev(points.end()); ++it) {
+			points2.push_back(AtoBProg(*it, *std::next(it, 1), t));
 		}
-		for (std::vector<Vec2>::iterator it = points2.begin(), second_to_last = --points2.end();
-			it != second_to_last; ++it) {
-			points3.push_back(AtoBProg(*it, *std::next(it, 1), progr));
+		for (std::vector<Vec2>::iterator it = points2.begin(); it != std::prev(points2.end()); ++it) {
+			points3.push_back(AtoBProg(*it, *std::next(it, 1), t));
 		}
-		curve.push_back(AtoBProg(points3.at(0), points3.at(1), progr));
-		progr += stepSize;
+		m_curve.push_back(AtoBProg(points3.at(0), points3.at(1), t));
 	}
-	return curve;
 }
